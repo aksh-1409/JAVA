c89d1e34bec7dc5814ab8208673bbda705997c85
Aksh Usrathe <akshusrathe007@gmail.com>
2025-05-21 13:14:49 +0530

    string

---
+++ 40_questions/ques6.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class ques6{
+
+
+    public staticvoid main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the limit: ");
+        int limit = sc.nextInt();
+
+        
+    }
+}
---
+++ string/Frequency_char.java
@@ -0,0 +1,26 @@
+class  Frequency_char{
+
+public static void main(String[] args){
+
+    String str = "aaaabbbccc";
+
+    StringBuilder sb = new StringBuilder();
+
+    int count =1;
+
+    for(int i=1;i<str.length();i++){
+
+        if(str.charAt(i)==str.charAt(i-1)){
+        count++;
+        }
+        else
+        {
+            sb.append(str.charAt(i-1)).append(count);
+            count = 1;
+        }
+       
+    }
+    sb.append(str.charAt(str.length()-1)).append(count);
+    System.out.println(sb.toString());
+}
+}
