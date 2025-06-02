766dcb53de3f6f62ff66820d85312eb343976f9a
Aksh Usrathe <akshusrathe007@gmail.com>
2025-06-02 10:46:02 +0530

    Comparator

---
+++ arraylist/Comparator.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+class Student implements Comparable <Student>{
+    int age;
+    String name;
+
+    public Student(int age, String name){
+        this.age = age;
+        this.name = name ;
+    }
+
+    public String toString(){
+        return age + " " +name;
+    }
+
+    public int compareTo(Student s){
+        return this.age-s.age;
+    }
+}
+
+class Comparator{
+
+    public static void main(String[] args){
+        ArrayList<Student> list = new ArrayList<>();
+        list.add(new Student(12, "rahul"));
+        list.add(new Student(11, "raju"));
+     
+            // ArrayList<Integer> list = new ArrayList<>();
+            // list.add(1);
+            // list.add(0);
+            // list.add(4);
+            Collections.sort(list);
+            for(Student s: list){
+                System.out.println(s.toString());
+            }
+            System.out.println(list);
+     
+     }
+}
