3c32cc77633c719f718e3992017268c1dfb405e2
Aksh Usrathe <akshusrathe007@gmail.com>
2025-06-11 11:30:22 +0530

    javaFileHandling

---
+++ javaFileHandling/ReadingAFile.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.File;
+
+class ReadingAFile{
+    public static void main(String[] args){
+    try{
+        File file = new File("example.txt");
+        if(file.exists()){
+            Scanner scanner = new Scanner(file);
+            while(scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+            scanner.close();
+        }else{
+            System.out.println("File does not exist.");
+        }
+    } catch(IOException e){
+        System.out.println("an error occured while reading the file");
+        e.printStackTrace();
+    }
+    }
+}
